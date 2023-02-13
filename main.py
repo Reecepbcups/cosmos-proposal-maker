70f1bd541a8579d2b110937635aa8cf39abf2cb9
Reece Williams <reecepbcups@gmail.com>
2023-02-13 16:30:50 -0600

    example

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 proposal_text = """
 # Juno v12 Upgrade Proposal
 
-To reduce spamming in the Juno governance system, we propose requiring a 20% deposit (200 JUNO) on proposal creation. This will ensure that only quality proposals are submitted and deter malicious governance spam. Should you wish to make a proposal, but don't have 200 Juno, you can reach out to a member of the core team or Sub-DAOs for assistance. The minimum deposit is meant to deter spam, not participation!
+To reduce spamming in the Juno governance system, we propose requiring a 20% deposit (200 JUNO) on proposal creation. This will ensure that only quality proposals are submitted and deter malicious governance spam. Should you wish to make a proposal, but don't have 200 Juno, you can reach out to a member of the core team or Sub-DAOs for assistance. The minimum deposit is meant to deter spam, not participation.
 
 Tag v12.0.0-beta.1 has been tested on our testnet, uni-6, and will be finalized as v12.0.0 if this proposal passes.
 
 **NOTE** This subsequently renames the previous [v12 upgrade to v13 from our medium blog post](https://medium.com/@reecepbcups/juno-v12-update-4bab64640a62)
+
+Provided this upgrade passes, it is scheduled for block [7075551](https://www.mintscan.io/juno/blocks/7075551). Roughly expected Monday, February 20th around 1700UTC.
 
 ## Voting Options
 
