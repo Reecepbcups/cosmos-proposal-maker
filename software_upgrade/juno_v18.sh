d4429b7750d01d1cc220c16d5efa7629c45849ee
Reece Williams <reecepbcups@gmail.com>
2023-10-24 14:45:31 -0500

    alpha.1

--- software_upgrade/juno_v18.sh
+++ software_upgrade/juno_v18.sh
@@ -29,7 +29,7 @@ FEES="500000$DENOM" # 0.5
 
 TITLE="Juno Upgrade: $UPGRADE_HUMAN_NAME ($UPGRADE_NAME)"
 
-DESC="# Juno Epona Upgrade (v18)\n\n[Release Notes](https://github.com/CosmosContracts/juno/releases/tag/v18.0.0)\n\n---\n\n\n## Voting Options\nYES: Agree to proceed with the v18 network upgrade.\nNO: Disagree with the v18 network upgrade."
+DESC="# Juno Epona Upgrade (v18)\n\n[Release Notes](https://github.com/CosmosContracts/juno/releases/tag/v18.0.0-alpha.1)\n\n---\n\n\n## Voting Options\nYES: Agree to proceed with the v18 network upgrade.\nNO: Disagree with the v18 network upgrade."
 UPGRADE_INFO='{"binaries":{"linux/amd64":"https://github.com/CosmosContracts/juno/releases/download/v18.0.0-alpha.1/junod"}}'
 
 junod tx gov submit-legacy-proposal software-upgrade $UPGRADE_NAME --title="$TITLE" --description="$DESC" --deposit="$DEPOSIT" --upgrade-height="$UPGRADE_HEIGHT" --from=$KEY --keyring-backend=$KEYRING --chain-id=$CHAIN_ID --broadcast-mode=sync --node=$JUNOD_NODE --no-validate --upgrade-info=$UPGRADE_INFO --fees=$FEES --gas=2000000
